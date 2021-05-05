084c514063a94dc16d6a09bcea8bd862ab806cf5
Janderson Barbeito <janderson.bds@live.com>
2021-05-04 21:09:17 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
-  return (
-    <div className="text-primary"><h1>Olá Mundo!</h1></div>
-  );
+	return (
+		<>
+			<NavBar />
+			<div className="container">
+				<h1 className="text-primary">Olá Mundo!</h1>
+
+				<DataTable />
+			</div>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
